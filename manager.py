db75d565790c6e4b73ea13af9e90897ce2459d57
Christian <christiansantoslima21@gmail.com>
2022-01-09 17:32:44 -0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -2,30 +2,65 @@ from discord.ext import commands
 import tweepy
 import asyncio
 from decouple import config
+import psycopg2
+from urllib.parse import urlparse
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 class Manager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+
+
     @commands.Cog.listener()
     async def on_ready(self):
-            print (f"I'm Ready! {self.bot.user}")
-           
+            print (f"I'm ready {self.bot.user}")
+            
+            
+            result = urlparse(os.getenv("DATABASE_URL"))
+
+            DB_USER = result.username
+            DB_PASSWORD = result.password
+            DB_NAME = result.path[1:]
+            DB_HOST = result.hostname
+            DB_PORT = result.port
+            
             
 
-            
-            
-            users_id = ['DIALOGUE_staff','uchiyama_yurina', 'miyamiya_Satsu', 'kyoka_moriya', 'takamura_ayaka', 'iizuka_mayu', 'nene_hieda']
 
+
+
+            con = psycopg2.connect(dbname=DB_NAME, user=DB_USER, host=DB_HOST, password=DB_PASSWORD, port=DB_PORT)
+            cur = con.cursor() 
+                     
+            cur.execute("SELECT id_idol FROM idols;")
+            tuples_array = cur.fetchall()
+            con.commit()
+            users_id = [ID for ID, in tuples_array]         
+               
+            
+            
             last_ids = {
 
                 }
-        
 
-            
+            for idol_id in users_id:
+                last_ids [idol_id] = []
+
+            cur.execute("SELECT * FROM twitter;")
+            con.commit()
+            all_posts = cur.fetchall()    
+            for idol_id, post_id in all_posts:
+                idol_posts = last_ids.get(idol_id)
+                if idol_posts:
+                    last_ids[idol_id].append(post_id)
+                else:
+                    last_ids [idol_id] = [post_id]
                 
                     
-            for i in users_id:
-                    last_ids[i] = 0
+           
 
 
             auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
@@ -48,22 +83,30 @@ class Manager(commands.Cog):
                 
 
                         cursor=tweepy.Cursor(api.user_timeline,id=user, tweet_mode="extended",).items(1)
-                        channel = self.bot.get_channel()
+                        channel = self.bot.get_channel(885246211446165526)
                     
                     
                         for i in cursor:
                         
-                            if i.id != last_ids[user]:
+                            if not str (i.id) in last_ids[user]:
                             
                             
-            
-                                a = i.id
-                                last_ids[user] = a
+                                
+                                a = str (i.id)
+                                
+                                
+                                
+                                cur.execute("INSERT INTO twitter VALUES (%s, %s);", (user,a))         
+                                con.commit()   
+                                
+                                last_ids[user].append(a)
+                                
 
             
-                                await channel.send(f'New Tweets!!! https://twitter.com/user/status/{a}')
-    
-    
+                                await channel.send(f'New post! https://twitter.com/{user}/status/{a}')
+   
+            con.close()
+  
 consumer_key=config('') 
 consumer_secret=config('') 
 access_token_key=config('') 
